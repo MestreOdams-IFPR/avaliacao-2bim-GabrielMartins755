a5f404aef25efd06cce8c0e35636318611ee2af0
GabrielMartins <senqueiogabriel@gmail.com>
2025-07-02 15:08:55 -0300

    finalizado com atraso

--- TesteForca.java
+++ TesteForca.java
@@ -7,17 +7,26 @@ public class TesteForca {
         while (sc.hasNext()) {
             int candidatos[] = new int[sc.nextInt()];
             int k = sc.nextInt();
-            for (int i : candidatos) {
+            for (int i = 0; i < candidatos.length; i++) {
                 candidatos[i] = sc.nextInt();
             }
-            
+
             selectionSort(candidatos);
+            inverterVetor(candidatos);
 
             int soma = 0;
             for (int i = 0; i < k; i++) {
                 soma += candidatos[i];
             }
             System.out.println(soma);
+        }
+    }
+
+    public static void inverterVetor(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
     }
 
