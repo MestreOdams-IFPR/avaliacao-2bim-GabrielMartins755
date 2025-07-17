235394af5f4a30a9a15917499c2272d7acbd279b
OdairMoreira <odair.moreira.souza>
2025-07-17 15:34:19 -0300

    Conceito C | Prof. Odair

--- Ex02.java
+++ Ex02.java
@@ -10,9 +10,11 @@ public class Ex02 {
     }
 
     public static int tribonacci(int n) {
-        if (n == 0 || n == 1) return 0;
-        if (n == 2) return 1;
+        if (n == 0 || n == 1)
+            return 0;
+        if (n == 2)
+            return 1;
 
-        return (n - 1)+ tribonacci(n - 2) + tribonacci(n - 3);
-    } 
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    }
 }
